a60e4bd5ef70f8c739b4f5b94e65b35227ce851e
Riddhita Gosai <riddhitagosai5@gmail.com>
2026-07-29 16:01:57 +0530

    Implement DatabaseHelper for song management

---
+++ module 6/session 7/task 5/dbhelper5.dart
@@ -0,0 +1,37 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+import '../task 5/model9.dart';
+
+
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  static Database? _database;
+  factory DatabaseHelper() => _instance;
+  DatabaseHelper._internal();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await openDatabase(
+      join(await getDatabasesPath(), 'songs_task5.db'),
+      onCreate: (db, version) => db.execute("CREATE TABLE songs(id INTEGER PRIMARY KEY, title TEXT)"),
+      version: 1,
+    );
+    return _database!;
+  }
+
+  Future<void> saveSongs(List<Song> songs) async {
+    final db = await database;
+    await db.delete('songs');
+    for (var song in songs) {
+      await db.insert('songs', song.toMap());
+    }
+  }
+
+  Future<List<Song>> getSongs() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('songs');
+    return maps.map((m) => Song.fromMap(m)).toList();
+  }
+}
