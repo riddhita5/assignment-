660b41f58b4b42669e68c3eae1f68f56999a11c2
Riddhita Gosai <riddhitagosai5@gmail.com>
2026-08-08 13:46:35 +0530

    Create  main.dart

---
+++ module 7/session 1/task 1/main.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Insta Clone Intro'),
+        ),
+        body: const Center(
+          child: Text(
+            'Firebase Initialized Successfully!',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
