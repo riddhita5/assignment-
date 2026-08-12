3112c8947814a2617ab060bf7729a41a4dbc647b
Riddhita Gosai <riddhitagosai5@gmail.com>
2026-08-12 16:19:21 +0530

    Add files via upload

---
+++ module 7/session 7/task 5/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'refactored_demo.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: RefactoredDemo(),
+  ));
+}
