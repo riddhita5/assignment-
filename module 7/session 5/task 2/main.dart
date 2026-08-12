2ebb28c8087d3bfe967dc41893ad0d3b3b86e548
Riddhita Gosai <riddhitagosai5@gmail.com>
2026-08-12 15:09:36 +0530

    Initialize Flutter app with Firebase integration

---
+++ module 7/session 5/task 2/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'restaurant_list.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const RestaurantApp());
+}
+
+class RestaurantApp extends StatelessWidget {
+  const RestaurantApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Restaurant List (Task 2)',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const RestaurantListScreen(),
+    );
+  }
+}
