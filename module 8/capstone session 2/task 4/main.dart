94a694ffff53a3b3926de3cc93ebdc834f4b12c4
Riddhita Gosai <riddhitagosai5@gmail.com>
2026-08-30 00:01:07 +0530

    Add files via upload

---
+++ module 8/capstone session 2/task 4/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'payment_crash_demo.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: PaymentCrashDemo(),
+  ));
+}
