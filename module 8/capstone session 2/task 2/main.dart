760208b3460e97b5d33de0216a377ea37dc50f06
Riddhita Gosai <riddhitagosai5@gmail.com>
2026-08-29 23:58:31 +0530

    Initialize Flutter app with ZomatoDetailsScreen

---
+++ module 8/capstone session 2/task 2/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'restaurant_details.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: ZomatoDetailsScreen(),
+  ));
+}
