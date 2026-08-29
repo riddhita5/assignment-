4c27f319e0494ff7974cdf49f9d185a985ed68d6
Riddhita Gosai <riddhitagosai5@gmail.com>
2026-08-29 23:59:53 +0530

    Add main.dart for Flutter app entry point

---
+++ module 8/capstone session 2/task 3/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'config_validator.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: ConfigValidatorScreen(),
+  ));
+}
