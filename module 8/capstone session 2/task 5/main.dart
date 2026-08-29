3bfcb1c8ab54ba5bef47f5204a6f2fca48aff3c7
Riddhita Gosai <riddhitagosai5@gmail.com>
2026-08-30 00:02:04 +0530

    Initialize Flutter app with PreReleaseChecklistScreen

---
+++ module 8/capstone session 2/task 5/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'pre_release_checklist.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: PreReleaseChecklistScreen(),
+  ));
+}
