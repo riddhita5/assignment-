45b767e8e20aa61550037b6b5f1188755f212e4d
Riddhita Gosai <riddhitagosai5@gmail.com>
2026-08-29 23:57:46 +0530

    Initialize Flutter app with FlipkartCartScreen

---
+++ module 8/capstone session 2/task 1/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'cart_screen.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: FlipkartCartScreen(),
+  ));
+}
