a2d9a93964969641ce74ef91ae435f5b300f22c5
Riddhita Gosai <riddhitagosai5@gmail.com>
2026-09-19 17:55:39 +0530

    Create a simple Flutter app with navigation
    This Dart file implements a simple Flutter application with two screens: MainScreen and CartScreen. The MainScreen contains a button that navigates to the CartScreen.

---
+++ module 9/session 1/task 1/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const MainScreen(),
+    );
+  }
+}
+
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const CartScreen(),
+              ),
+            );
+          },
+          child: const Text('Go to Cart'),
+        ),
+      ),
+    );
+  }
+}
+
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cart Screen'),
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to Cart',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
