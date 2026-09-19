ecbc088fc3cecfb2f86a73bf7edf2bee939176ae
Riddhita Gosai <riddhitagosai5@gmail.com>
2026-09-19 18:31:01 +0530

    Initialize MyMusicApp with routing setup

---
+++ module 9/session 2/task 5/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'app_routes.dart';
+import 'screens/home_screen.dart';
+import 'screens/playlist_screen.dart';
+import 'screens/settings_screen.dart';
+import 'screens/favourites_screen.dart';
+
+void main() {
+  runApp(const MyMusicApp());
+}
+
+class MyMusicApp extends StatelessWidget {
+  const MyMusicApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Music App',
+
+      routes: {
+        AppRoutes.home: (context) => const HomeScreen(),
+        AppRoutes.playlist: (context) => const PlaylistScreen(),
+        AppRoutes.settings: (context) => const SettingsScreen(),
+        AppRoutes.favourites: (context) => const FavouritesScreen(),
+      },
+
+      initialRoute: AppRoutes.home,
+    );
+  }
+}
